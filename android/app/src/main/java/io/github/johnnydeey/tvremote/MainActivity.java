7d1ca7b60aa7c60e5473fa7a75f8334e30128203
JohnnyDeey <101556340+JohnnyDeey@users.noreply.github.com>
2026-08-19 23:05:39 +0200

    Initial TV Remote Android app

---
+++ android/app/src/main/java/io/github/johnnydeey/tvremote/MainActivity.java
@@ -0,0 +1,25 @@
+package io.github.johnnydeey.tvremote;
+
+import android.os.Bundle;
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webView.setWebViewClient(new BridgeWebViewClient(getBridge()) {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+    }
+}
